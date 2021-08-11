aaec184b33400964a618478ae500c7cdd33b46f6
rephlexx <37312905+rephlexx@users.noreply.github.com>
2021-08-11 11:32:18 +0200

    Add files via upload

---
+++ First try.py
@@ -0,0 +1,6 @@
+#Print string
+print('Hello world') #single quotes
+print("Hello world") #double quotes
+print("""Hello world
+multiple lines""") #triple quote for multi line
+print("this string is "+"awesome!") #we can also concatenate
