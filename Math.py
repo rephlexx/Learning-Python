f3eb85800890fa7fef7cada57b51ac21f94c7a6f
rephlexx <37312905+rephlexx@users.noreply.github.com>
2021-08-11 11:31:27 +0200

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,10 @@
+#Math
+print(50 + 50) #add
+print(50 - 50) #subtract
+print(50 * 50) #multiply
+print(50 / 50) #divide
+print(50 + 50 - 50 * 50 / 50) #PEMDAS
+print(50 ** 2) #exponents
+print(50 % 6) #modulo leftover 2 as result
+print(50 / 6)
+print(50 // 6) #no leftovers
