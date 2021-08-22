a4062d84658bdad6b4050fb3fe4b2aaee71fd147
rephlexx <37312905+rephlexx@users.noreply.github.com>
2021-08-22 23:12:57 +0200

    Add files via upload

---
+++ Relational and Boolean Operater.py
@@ -0,0 +1,12 @@
+#Relational and Boolean operators
+greater_than = 7 > 5
+less_than = 5 < 7
+greater_than_equal_to = 7 >= 7
+less_than_equal_to = 7 <= 7
+
+test_and = (7 > 5) and (5 < 7) #True
+test_and_2 = (7 > 5) and (5 < 7) #False
+test_or = (7 > 5) or (5 < 7) #True
+test_or2 (7 > 5 ) or (5 > 7) #True
+
+test_not = not True #False   Check Truth tables 
