441c8fe3e899c6c3d907461c82f6e0f2d2fdf71f
rephlexx <37312905+rephlexx@users.noreply.github.com>
2021-08-22 23:14:16 +0200

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,38 @@
+#Functions
+
+print('\n') #next line
+
+#Functions
+print("here is an example function")
+
+def who_am_i(): #this is a function
+        name = "Michael" #Variable
+        age = 30 #Variable
+        print("My name is " + name + " and I am " + str(age) + " years old.")
+who_am_i()
+
+#Adding parameters
+def add_one_hundred(num):
+                print(num + 100)
+add_one_hundred(100)
+
+#Multiple parameters
+def add(x,y):
+        print(x + y)
+add(7,7)
+def multiply(x,y):
+        return x * y  #Returning a number to call it later
+print(multiply(7,7))
+print(7*7)
+
+def square_root(x):
+        print(x ** .5)
+
+square_root(64)
+
+def new_line():
+    print('\n')
+new_line()
+
+
+
